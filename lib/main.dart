5d816e3eff34ffaa50650d2ada5eef15e15132d3
Chau Ka Leung <lacusleung012@gmail.com>
2022-11-12 23:03:41 +0800

    add question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    // print(_questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animarl?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('My App')),
+          body: Column(
+            children: [
+              Text(questions[_questionIndex]),
+              ElevatedButton(
+                onPressed: _answerQuestion, child: Question('Answer 1')
+              ),
+              ElevatedButton(
+                onPressed: _answerQuestion, child: Text('Answer 2')
+              ),
+              ElevatedButton(
+                onPressed: _answerQuestion, child: Text('Answer 3')
+              )
+            ],
+          )),
+    );
   }
 }
